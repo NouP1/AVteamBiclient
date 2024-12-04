fb0626ef3b2e7af521f494fbaa3bd932c562fbb4
NouP1 <smolen43ret@gmail.com>
2024-12-04 16:27:55 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,19 @@ const App = () => {
     if (savedUser) {
       setUser(JSON.parse(savedUser));
     }
+    const checkForReload = async () => {
+      const response = await fetch('/api/reload');
+      const data = await response.json();
+      if (data.reload) {
+        window.location.reload();
+      }
+    };
+  
+    const interval = setInterval(checkForReload, 5000); // Проверяем каждые 5 секунд
+  
+    return () => clearInterval(interval); // Очистка при размонтировании компонента
   }, []);
+ 
 
   const handleLogin = (loggedInUser) => {
     localStorage.setItem('user', JSON.stringify(loggedInUser));
